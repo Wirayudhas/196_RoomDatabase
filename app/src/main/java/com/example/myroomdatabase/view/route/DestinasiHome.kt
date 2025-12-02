9c6be94c8fcbc92667306676d8181a3c48f4f865
[Arthur Gerald] <[arthur.gerald.ft23@mail.umy.ac.id]>
2025-12-02 09:40:50 +0700

    menambahkan isi file yang sudah dibuat

--- app/src/main/java/com/example/myroomdatabase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/myroomdatabase/view/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.myroomdatabase.view.route
 
-object DestinasiHome {
+import com.example.myroomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
