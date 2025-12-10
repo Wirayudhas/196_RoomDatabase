d0c27b535023fe67d37a0e4a1c03fa9769bcc8e3
[Arthur Gerald] <[arthur.gerald.ft23@mail.umy.ac.id]>
2025-12-11 00:32:51 +0700

    menambahkan isi SiswaDao.kt

--- app/src/main/java/com/example/myroomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroomdatabase/room/SiswaDao.kt
@@ -1,16 +1,29 @@
 package com.example.myroomdatabase.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
-    @Query("SELECT * FROM Tblsiswa ORDER BY nama ASC")
+
+    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    // Edit : tambah 3 fungsi berikut
+    @Query("SELECT * FROM tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa?>
+
+    @Update
+    suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
