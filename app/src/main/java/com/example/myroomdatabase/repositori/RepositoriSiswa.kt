9a7fff0a1af887d906d3bdde9d2df1844f0e050a
[Arthur Gerald] <[arthur.gerald.ft23@mail.umy.ac.id]>
2025-12-11 00:32:35 +0700

    menambahkan isi RepositoriSiswa.kt

--- app/src/main/java/com/example/myroomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomdatabase/repositori/RepositoriSiswa.kt
@@ -1,6 +1,5 @@
 package com.example.myroomdatabase.repositori
 
-
 import com.example.myroomdatabase.room.Siswa
 import com.example.myroomdatabase.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
@@ -8,11 +7,27 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
 ) : RepositoriSiswa {
-    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override fun getAllSiswaStream(): Flow<List<Siswa>> =
+        siswaDao.getAllSiswa()
+
+    override suspend fun insertSiswa(siswa: Siswa) =
+        siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> =
+        siswaDao.getSiswa(id)
+
+    override suspend fun deleteSiswa(siswa: Siswa) =
+        siswaDao.delete(siswa)
+
+    override suspend fun updateSiswa(siswa: Siswa) =
+        siswaDao.update(siswa)
 }
